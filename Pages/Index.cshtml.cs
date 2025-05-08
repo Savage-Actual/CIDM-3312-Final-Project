ef3099b069e3c525e1c0cae74238d04131893173
Dakota Sturgeon <djsturgeon1@buffs.wtamu.edu>
2025-05-08 15:43:36 -0500

    update 3

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CIDM_3312_Final_Project.Models;
+using CIDM_3312_Final_Project.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace CIDM_3312_Final_Project.Pages;
-
-public class IndexModel : PageModel
+namespace CIDM_3312_Final_Project.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly AppDbContext _context;
 
-    public void OnGet()
-    {
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
+        }
 
+        public List<Weapon> FeaturedWeapons { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            FeaturedWeapons = await _context.Weapons
+                .Include(w => w.Manufacturer)
+                .OrderByDescending(w => w.DamageRating)
+                .Take(5)
+                .ToListAsync();
+        }
     }
 }
+
--- obj/Debug/net9.0/CIDM-3312-Final-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/CIDM-3312-Final-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIDM-3312-Final-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada91c245475856c0088c388fa25ac0e878160b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8c8bb8722db95d548434c352dc0cb511ba8b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIDM-3312-Final-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIDM-3312-Final-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
